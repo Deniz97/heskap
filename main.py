1020ab9ff174ccaae4e76c275a0d94e7a8171e06
DENIZ RASIM ULUG <e2172088@ceng.metu.edu.tr>
2018-03-01 21:14:26 +0000

    first main.py commit

--- main.py
+++ main.py
@@ -1,3 +1,15 @@
+import ride
+
+
+r,c, vehicle_count, ride_conunt, bonus, time_steps = tuple(input().split(" "))
+
+rides = []
+
+for i in range(ride_conunt):
+	start = [] #start location
+	finish = [] #finish location
+	start[0],start[1],finish[0],finish[1], earliest_start, latest_finish = tuple(input().split(" "))
+	rides.append( ride.ride(start,finish,earliest_start,latest_finish) )
 
 
 
