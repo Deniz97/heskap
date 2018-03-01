1ef3bc61cc60ead93f09cf21ab870f2b1eaad577
Oğuz Vuruşkaner <oguz@localhost.localdomain>
2018-03-01 21:14:47 +0300

    Car classı yenilendi ve ride classına başlandı.

--- ride.py
+++ ride.py
@@ -1,2 +1,20 @@
 class Ride:
-	def __init__(self,):
+	def __init__(self,frm,to,earliestStart,latestFinish):
+		self.frm = frm
+		self.to = to
+		self.earliestStart = earliestStart
+		self.latestFinish = latestFinish
+
+
+	def from(self):
+		return self.frm
+
+	def to(self):
+		return self.to
+
+	def earliestStart(self):
+		return self.earliestStart
+
+	def latestFinish(self):
+		return self.latestFinish
+	
