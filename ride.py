756cd683cd8ee69be7298ce74d20770cdcfe6b4e
Onur Tirtir <onur.tirtir@ceng.metu.edu.tr>
2018-03-01 22:26:08 +0300

    ride is edited

--- ride.py
+++ ride.py
@@ -1,11 +1,15 @@
 class Ride:
-	def __init__(self,frm,to,earliestStart,latestFinish,rideNumber):
+	def __init__(self,frm,to,earliestStart,latestFinish,rideNumber, bonus):
 		self.frm = frm
 		self.to = to
 		self.earliestStart = earliestStart
 		self.latestFinish = latestFinish
 		self.rideNumber = rideNumber
 		self.isTaken = False
+
+                tmpScore = abs(frm[0] - to[0]) + abs(frm[1] - to[1]) ## it is really temp
+                self.score = (tmpScore, tmpScore + bonus)
+                # a tuple of score w/o bonus and score w/ bonus
 
 	def from(self):
 		return self.frm
@@ -25,3 +29,17 @@ class Ride:
 	#Rideın alınıp alınmadığını gösteriyor.
 	def taken(self):
 		self.isTaken = True
+    
+        # return tuple of rate w/o bonus & w/ bonus & rideNumber
+        def calcScore4Car(self, car, curTime):
+                penalty = abs(frm[0] - car.coords[0]) + abs(frm[1] - car.coords[1])
+               
+                if curTime > self.earliestStart:
+                    return (self.score[0] / penalty, self.score[0] / penalty, self.rideNumber)
+                else:
+                    return (self.score[1] / penalty, self.score[1] / penalty, self.rideNumber)
+        
+
+                    
+
+
